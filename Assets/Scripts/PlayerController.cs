10898328a4ce6b54d1d52e8d2a8eaae00c196fe8
mynameisfloog <48299534+mynameisfloog@users.noreply.github.com>
2020-05-18 16:46:56 -0400

    Camera locks on player + movement disabled when standing on nothing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,36 +6,41 @@ public class PlayerController : MonoBehaviour
 {
     public float distance;
     bool moveW; bool moveE; bool moveN; bool moveS;
+    bool canMove = true;
+    // For the sake of the last line being simple, pretend true and false have reverse meanings
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        if (canMove)
         {
-            transform.position -= new Vector3(distance, 0); moveW = false;
-        }
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            transform.position += new Vector3(distance, 0); moveE = false;
-        }
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            transform.position += new Vector3(0, distance); moveN = false;
-        }
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            transform.position -= new Vector3(0, distance); moveS = false;
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                transform.position -= new Vector3(distance, 0); moveW = false;
+            }
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                transform.position += new Vector3(distance, 0); moveE = false;
+            }
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                transform.position += new Vector3(0, distance); moveN = false;
+            }
+            if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                transform.position -= new Vector3(0, distance); moveS = false;
+            }
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("THING");
         if (collision.gameObject.GetComponent<TileController>().burnt == false) 
         {
             if (!moveW) moveW = true;
             if (!moveE) moveE = true;
             if (!moveN) moveN = true;
             if (!moveS) moveS = true;
+            canMove = true;
         }
         else
         {
@@ -44,5 +49,10 @@ public class PlayerController : MonoBehaviour
             if (!moveN) { transform.position -= new Vector3(0, distance); moveN = true; Debug.Log("North"); }
             if (!moveS) { transform.position += new Vector3(0, distance); moveS = true; Debug.Log("South"); }
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        canMove = false;
     }
 }
