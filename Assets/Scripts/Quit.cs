0bdc92fb58e0e3a576807946ae5372729339fdc3
mynameisfloog <48299534+mynameisfloog@users.noreply.github.com>
2020-05-23 18:26:25 -0400

    DONE DONE DONE DONE
    IT'S DONE HERE'S THE BIG DONE

---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    public void OnQuit()
+    {
+        Application.Quit();
+        Debug.Log("Game is Quitting");
+    }
+}
--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -8,10 +8,12 @@ public class StartButton : MonoBehaviour
     public GameObject logo;
     public GameObject h2p;
     public GameObject space;
+    public GameObject quit;
 
     public void PressStart()
     {
         logo.SetActive(false);
+        quit.SetActive(false);
         h2p.SetActive(true);
         space.SetActive(true);
         start.SetActive(false);
